353907f8ce8ca2851526ca4cc923a70033f81c94
SistemasFigsac <sistemasfigsac@gmail.com>
2026-03-07 12:57:46 -0500

    feat: actualizar masivo por estado

--- api/services/updateService.py
+++ api/services/updateService.py
@@ -34,3 +34,37 @@ def guardarHistorial(tabla_nombre,datos_dict,filtro_dict):
     if tabla_nombre == "datosgeneralesdelproceso" and "estado" in datos_dict:
         return guardarHistorialRequerimientos(datos_dict,filtro_dict, False)
     
+
+def actualizarDatosMasivo(tabla_nombre, datos_dict, filtro_dict):
+    with transaction.atomic():
+
+        modelo = apps.get_model('api', tabla_nombre)
+        if not modelo:
+            raise_error("E004", f"No se encontro el modelo '{tabla_nombre}' en la api")
+
+        queryset = modelo.objects.filter(**filtro_dict)
+
+        if not queryset.exists():
+            raise_error("E404", "No se encontraron registros para actualizar")
+
+        campos_modelo = [field.name for field in modelo._meta.get_fields()]
+
+        for objeto in queryset:
+
+            # Manejo especial de ediciones
+            if "ediciones" in campos_modelo and "ediciones" in datos_dict:
+                texto_nuevo = datos_dict["ediciones"]
+                texto_anterior = getattr(objeto, "ediciones", "") or ""
+                setattr(objeto, "ediciones", f"{texto_anterior}{texto_nuevo}")
+
+            # Actualizar demás campos
+            for campo, valor in datos_dict.items():
+                if campo != "ediciones":
+                    setattr(objeto, campo, valor)
+
+            objeto.save()
+
+        return True
+    
+    return True
+    
